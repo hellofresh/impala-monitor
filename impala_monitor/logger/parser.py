74435be6d934d3a482b8a12e244498d490c34571
Sergio Sola <introduccio@gmail.com>
2017-03-01 18:23:28 +0100

    all parsers done

--- impala_monitor/logger/parser.py
+++ impala_monitor/logger/parser.py
@@ -1,5 +1,21 @@
+import re
 from bs4 import BeautifulSoup
 from datetime import datetime
+
+
+class Query(object):
+    def __init__(self, query: dict):
+        self.query = query
+
+    def __getattr__(self, item):
+        if item not in self.query:
+            raise ValueError("Item {} does not exists".format(item))
+
+        return self.query[item]
+
+    # @property
+    # def query(self):
+    #     return self.query
 
 
 class ImpalaQueryLogParser(object):
@@ -30,16 +46,59 @@ class ImpalaQueryLogParser(object):
             if query_type not in ['QUERY'] or not query_state in ['FINISHED']:
                 continue
 
-            queries.append({
+            start_time = datetime.strptime(
+                cells[4].get_text(), "%Y-%m-%d %H:%M:%S.%f000"
+            )
+
+            end_time = datetime.strptime(
+                cells[5].get_text(), "%Y-%m-%d %H:%M:%S.%f000"
+            )
+
+            execution_time = end_time - start_time
+
+            query_id = self.extract_query_id(
+                cells[9].find('a', href=True).get('href')
+            )
+
+            queries.append(Query({
                 'query': cells[2].get_text(),
                 'query_type': query_type,
                 'state': query_state,
                 'fetched_rows': int(cells[8].get_text()),
                 'user': cells[0].get_text(),
-                'start_time': datetime.strptime(cells[4].get_text(),
-                                                "%Y-%m-%d %H:%M:%S.%f000"),
-                'end_time': datetime.strptime(cells[5].get_text(),
-                                              "%Y-%m-%d %H:%M:%S.%f000"),
-            })
+                'start_time': start_time,
+                'end_time': end_time,
+                'execution_time': execution_time,
+                'query_id': query_id
+            }))
 
         return queries
+
+    def extract_profile(self, query: Query) -> Query:
+        profile = self.soup.findAll('div', {'class': 'container'})[1].find(
+            'pre').get_text()
+
+        memory_allocated = re.search('Memory=([0-9\.GB]+)', profile).group(1)
+        vcores_allocated = re.search('VCores=([0-9]+)', profile).group(1)
+
+        query = query.query
+        query['memory_allocated'] = memory_allocated
+        query['vcores_allocated'] = vcores_allocated
+
+        return Query(query)
+
+    @staticmethod
+    def extract_query_id(link: str) -> str:
+        """
+        Given a link it extracts the query id
+        :param link:
+        :return:
+        """
+        regex = 'query_id=([a-z0-9\:]+)'
+
+        matches = re.search(regex, link)
+
+        if not matches:
+            return None
+
+        return matches.group(1)
--- tests/logger/parser_test.py
+++ tests/logger/parser_test.py
@@ -2,6 +2,7 @@ import os
 from unittest import TestCase
 from datetime import datetime
 from impala_monitor.logger.parser import ImpalaQueryLogParser
+from impala_monitor.logger.parser import Query
 
 
 class HtmlLoader(object):
@@ -24,6 +25,13 @@ class HtmlLoader(object):
 
 
 class ImpalaQueryLogParserTest(TestCase):
+    def test_extract_query_id(self):
+        query_id = ImpalaQueryLogParser.extract_query_id(
+            '/query_profile?query_id=dasdsa8798das8798das:dsa'
+        )
+
+        self.assertEqual(query_id, 'dasdsa8798das8798das:dsa')
+
     def test_get_queries(self):
         html = HtmlLoader().load('impala_queries.html')
         parser = ImpalaQueryLogParser(html)
@@ -33,5 +41,18 @@ class ImpalaQueryLogParserTest(TestCase):
         self.assertEqual(20, len(queries))
 
         for query in queries:
-            self.assertIsInstance(query['start_time'], datetime)
-            self.assertIsInstance(query['end_time'], datetime)
+            self.assertIsInstance(query, Query)
+            self.assertIsInstance(query.start_time, datetime)
+            self.assertIsInstance(query.end_time, datetime)
+
+    def test_extract_profile_data(self):
+        html = HtmlLoader().load('impala_query_profile.html')
+        parser = ImpalaQueryLogParser(html)
+
+        query = Query({'query_id': '1234'})
+
+        query = parser.extract_profile(query)
+
+        self.assertEqual('2.13GB', query.memory_allocated)
+        self.assertEqual('2', query.vcores_allocated)
+        self.assertEqual('1234', query.query_id)
